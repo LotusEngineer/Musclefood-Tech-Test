8669719cc1bcf4eb86fcb2917783f5255ed5df2e
Ryan Burke <Ryan.burke@firststepsjs.com>
2020-07-21 20:47:06 +0100

    RED: Added to test to cover inserting dime

--- src/index.test.ts
+++ src/index.test.ts
@@ -12,11 +12,20 @@ describe("Vending Machine", () => {
     });
 
     describe("When a nickel has been inserted", () => {
-      it("should update the current amount and display amount", () => {
+      it("should increase the current amount by 0.05 and display amount", () => {
         //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
         const nickel: Coin = new Coin(5, 21.21, 1.95);
         subject.insertCoin(nickel);
         expect(subject.viewDisplay()).toBe("0.05");
+      });
+    });
+
+    describe("When a dime is inserted", () => {
+      it("should increase the current amount by 0.10 and display the current value", () => {
+        //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
+        const dime: Coin = new Coin(2.268, 17.91, 1.35);
+        subject.insertCoin(dime);
+        expect(subject.getDisplay()).toBe("0.10");
       });
     });
   });
