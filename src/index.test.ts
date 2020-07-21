dcc6bff3c5fb2e73326537da285d26b7e2914462
Ryan Burke <Ryan.burke@firststepsjs.com>
2020-07-21 20:45:11 +0100

    REFACTOR: Refactor tests to init subject in beforeeach

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,17 @@
 import VendingMachine, { Coin } from "./index";
 describe("Vending Machine", () => {
+  let subject;
+  beforeEach(() => {
+    subject = new VendingMachine();
+  });
   describe("Accept Coins", () => {
     describe("When no coin has been inserted", () => {
       it("should display INSERT COIN", () => {
-        const subject = new VendingMachine();
-
         expect(subject.viewDisplay()).toBe("INSERT COIN");
       });
     });
 
     describe("When a nickel has been inserted", () => {
-      const subject = new VendingMachine();
-
       it("should update the current amount and display amount", () => {
         //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
         const nickel: Coin = new Coin(5, 21.21, 1.95);
