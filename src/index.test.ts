dfaae703d5591a0bc261b4ddecc883fa61d4e9e4
Ryan Burke <Ryan.burke@firststepsjs.com>
2020-07-21 20:53:05 +0100

    REFACTOR: Refactor tests to define coins at top of testing scope for reuse

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
 import VendingMachine, { Coin } from "./index";
 describe("Vending Machine", () => {
   let subject;
+  //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
+  const nickel: Coin = new Coin(5, 21.21, 1.95);
+  const dime: Coin = new Coin(2.268, 17.91, 1.35);
+  const quarter: Coin = new Coin(5.67, 24.26, 1.75);
   beforeEach(() => {
     subject = new VendingMachine();
   });
@@ -13,8 +17,6 @@ describe("Vending Machine", () => {
 
     describe("When a nickel has been inserted", () => {
       it("should increase the current amount by 0.05 and display amount", () => {
-        //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
-        const nickel: Coin = new Coin(5, 21.21, 1.95);
         subject.insertCoin(nickel);
         expect(subject.viewDisplay()).toBe("0.05");
       });
@@ -22,8 +24,6 @@ describe("Vending Machine", () => {
 
     describe("When a dime is inserted", () => {
       it("should increase the current amount by 0.10 and display the current value", () => {
-        //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
-        const dime: Coin = new Coin(2.268, 17.91, 1.35);
         subject.insertCoin(dime);
         expect(subject.viewDisplay()).toBe("0.10");
       });
@@ -31,8 +31,6 @@ describe("Vending Machine", () => {
 
     describe("When a quarter is inserted", () => {
       it("should increase the current amount by 0.25 and display the current value", () => {
-        //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
-        const quarter: Coin = new Coin(5.67, 24.26, 1.75);
         subject.insertCoin(quarter);
         expect(subject.viewDisplay()).toBe("0.25");
       });
