99507905f2fe8e049e0460f105f7ee51d706c118
Ryan Burke <Ryan.burke@firststepsjs.com>
2020-07-21 20:26:56 +0100

    RED: Added to test to cover inserting nickel

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,5 +8,15 @@ describe("Vending Machine", () => {
         expect(subject.viewDisplay()).toBe("INSERT COIN");
       });
     });
+
+    describe("When a nickel has been inserted", () => {
+      const subject = new VendingMachine();
+
+      it("should update the current amount and display amount", () => {
+        //Weight (g), diameter(mm) & thickness(mm), referenced from US mint
+        subject.insertCoin(5, 21.21, 1.95);
+        expect(subject.viewDisplay()).toBe("0.05");
+      });
+    });
   });
 });
